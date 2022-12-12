a3047599df481a321b3002b7f923c2f111d20603
iiAhmedYT <61851106+iiAhmedYT@users.noreply.github.com>
2022-12-12 14:33:17 +0200

    Fix maven oopses

--- VersionSupport-1_19_R2/src/main/java/dev.iiahmed.mvs/MVS1_19_R2.java
+++ VersionSupport-1_19_R2/src/main/java/dev.iiahmed.mvs/MVS1_19_R2.java
@@ -136,7 +136,7 @@ public class MVS1_19_R2 extends DisguiseProvider {
             ep.connection.send(new ClientboundRespawnPacket(ep.getLevel().dimensionTypeId(),
                     ep.getLevel().dimension(),
                     seed, ep.gameMode.getGameModeForPlayer(),
-                    ep.gameMode.getGameModeForPlayer(), false, false, ClientboundRespawnPacket.KEEP_ATTRIBUTES,
+                    ep.gameMode.getGameModeForPlayer(), false, false, ClientboundRespawnPacket.KEEP_ALL_DATA,
                     ep.getLastDeathLocation()));
             player.teleport(location);
             ep.connection.send(new ClientboundPlayerInfoUpdatePacket(
